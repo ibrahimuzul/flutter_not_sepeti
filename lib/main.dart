ad3c6a368b08f54e17d63587d4638fa993bcde3d
iuzul <iuzul@IBRAHIMUZUL>
2020-10-01 09:58:32 +0300

    137/138/139 Databasehelper Sınıfı ve Veritabanının Assetten Uretilmesi Guncelleme Lutfen Izleyin Kategori ve Not Modellerinin Oluşturulması

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,53 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Not Sepeti',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Text("test"),
+      home: NotListesi(),
     );
   }
 }
+
+class NotListesi extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Center(child: Text("Not Sepeti"),),),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            onPressed: () {
+              //kategoriEkleDialog(context);
+            },
+            heroTag: "KategoriEkle",
+            tooltip: "Kategori Ekle",
+            child: Icon(
+              Icons.import_contacts,
+              color: Colors.white,
+            ),
+            mini: true,
+          ),
+          FloatingActionButton(
+            tooltip: "Not Ekle",
+            heroTag: "NotEkle",
+            onPressed: () {
+              //_detaySayfasinaGit(context);
+            },
+            child: Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+      body:Container(),
+    );
+  }
+}
+
 
 
